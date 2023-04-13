a65652b81dd48bccb89c5a58ca425867a684aea1
RyangGosling <kpliaja@localhost.localdomain>
2023-04-13 10:17:25 +0300

    first commit

--- dwm/status config/config.h
+++ dwm/status config/config.h
@@ -65,5 +65,8 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+        { run_command," [%s] ", "pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | head -1"},
+        { battery_perc, "[B%s] ", "BAT0" },
+        { keymap,   "[%s] ",           NULL    },
+	{ datetime, "[%s]" ,           "%d.%m %R" },
 };
